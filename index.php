dc07945c2d429a4a707336ab16a420d93059404d
Akbar Eka Saputra <saputraakbar720@gmail.com>
2025-10-19 20:18:21 +0700

    index.php

--- index.php
+++ index.php
@@ -3,7 +3,14 @@ $apiKey = "571c73b632fa4635863641064ba9f355";
 $category = isset($_GET['category']) ? $_GET['category'] : 'technology';
 $url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=571c73b632fa4635863641064ba9f355";
 
-$response = file_get_contents($url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+  die("cURL Error: " . curl_error($ch));
+}
+curl_close($ch);
 $data = json_decode($response, true);
 ?>
 <!DOCTYPE html>
